eca4783fe40eedf0ca2e64d9c59fd7f3693edd2d
eveoliv <everton_ace@hotmail.com>
2019-08-20 22:58:08 -0300

    Inclusao da custom.css e alteracao do layout principal

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -2,13 +2,30 @@
 <head> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link href="/css/custom.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>Controle de Estoque</title>
 </head>
 <body>
     <div class="container mt-5">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/produtos">
+                Estoque Laravel
+                </a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+            <li><a href="/produtos">Listagem</a></li>
+            </ul>
+        </div>
+    </nav>
         @yield('conteudo')
+
+        <footer class="footer">
+            <p>© Livro de Laravel da Casa do Código.</p>
+        </footer>
     </div>
 </body>
 </html>
